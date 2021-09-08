11cfced7075bdba92daecc91cbfa5e31754ac41c
Tomas Vesely <turican0@gmail.com>
2021-09-08 15:28:42 +0200

    210908-02

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
+REM xx
 cd c:\prenos\NeuralFin
-c:\prenos\NeuralFin\x64\Debug\neurnet.exe yes FB GOOG MSFT AMZN SE MELI FTNT
+REM c:\prenos\NeuralFin\x64\Debug\neurnet.exe yes FB GOOG MSFT AMZN SE MELI FTNT
 c:\prenos\NeuralFin\x64\Debug\neurnet.exe yes GOOG FB MSFT AMZN SE MELI FTNT
 c:\prenos\NeuralFin\x64\Debug\neurnet.exe yes MSFT FB GOOG AMZN SE MELI FTNT
 c:\prenos\NeuralFin\x64\Debug\neurnet.exe yes AMZN FB GOOG MSFT SE MELI FTNT
